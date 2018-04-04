6e58ba69ceb7378b73fdc93b27c62d81cf8b516a
Hackbright Student <no-reply@hackbrightacademy.com>
2018-04-04 15:41:58 -0700

    added subtract, mult, divide, square, cube, pow, mod functions

--- calculator.py
+++ calculator.py
@@ -16,14 +16,35 @@ calculator program yourself in this file.
 from arithmetic import *
 
 def calculator():
-        while True:
-            user_input = raw_input("> ")
-            token = user_input.split(' ')
-            if token[0] == "q":
-                return
-            elif token[0] == "+":
-                result = add(float(token[1]), float(token[2]))  
-                print result
+    while True:
+        user_input = raw_input("> ")
+        token = user_input.split(' ')
+        if token[0] == "q":
+            return
+        elif token[0] == "+":
+            result = add(float(token[1]), float(token[2]))
+            print result
+        elif token[0] == "-":
+            result = subtract(float(token[1]), float(token[2]))
+            print result
+        elif token[0] == "*":
+            result = multiply(float(token[1]), float(token[2]))
+            print result
+        elif token[0] == "/":
+            result = divide(float(token[1]), float(token[2]))
+            print result
+        elif token[0] == "square":
+            result = square(float(token[1]))
+            print result
+        elif token[0] == "cube":
+            result = cube(float(token[1]))
+            print result
+        elif token[0] == "pow":
+            result = power(float(token[1]), float(token[2]))
+            print result
+        elif token[0] == "mod":
+            result = mod(float(token[1]), float(token[2]))
+            print result
 
 
 calculator()
